65a90a6eb4a8fdb6391c708113db9c5441e12b13
Antoine Delia <antoine.delia@gmail.com>
2024-05-01 15:54:58 +0200

    fix: add credentials to prevent 403 errors

--- README.md
+++ README.md
@@ -14,12 +14,17 @@ You should have the [Serverless Framework](https://www.serverless.com/) and `pip
 Also, be sure that the email address used to send the mail is verified in AWS SES. 
 The mails for the recipients also have to be verified if you are in AWS SES sandbox (see [Troubleshooting](#troubleshooting)).
 
+For Reddit, you will need to [create a new Script App](https://www.reddit.com/prefs/apps/). This will give you a `client_id` and a `client_secret`, which are required.
+
 ## Configuration
 
 You can configure some parameters directly into the `serverless.yml` file:
 
 - **SENDER_MAIL:** The sender of the mail. Must be verified in AWS SES. Required.
 - **RECIPIENTS_MAILS:** The recipient of the mail. Can be a list by separating values with commas. Required.
+- **USER_AGENT:** The User-Agent that will be used in the headers when reaching Reddit. Required.
+- **REDDIT_CLIENT_ID:** The client id of your Reddit app. Required.
+- **REDDIT_CLIENT_SECRET:** The client secret of your Reddit app. Required.
 - **SUBREDDITS:** The list of subreddits you want the script to check, must be separated by a comma. Optional (default: pics,videos).
 - **POST_LIMIT:** The number of posts for each subreddit you want to send. Optional (default: 10).
 
--- src/reddit_weekly.py
+++ src/reddit_weekly.py
@@ -1,20 +1,22 @@
 # Given a list of subreddits, send an email containing the top posts from each of them every week
 
-from src.reddit import Post
-import requests
-import os
+import base64
 from datetime import datetime
-import boto3
-import logging
 import json
+import logging
+import os
+
+import boto3
 from botocore.exceptions import ClientError
+import requests
+
+from src.reddit import Post
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 
 logger = logging.getLogger()
 log_level = logging.getLevelName(LOG_LEVEL)
 logger.setLevel(log_level)
-logger.handlers[0].setFormatter(logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s\n'))
 logging.getLogger('boto3').setLevel(logging.ERROR)
 logging.getLogger('botocore').setLevel(logging.ERROR)
 
@@ -46,35 +48,57 @@ ses_client = boto3.client("ses")
 # Reddit Configuration
 ###
 
-reddit_api_url = "https://www.reddit.com/r/{subreddit}/top.json?sort=top&t=week&limit={limit}"
+reddit_api_url = "https://oauth.reddit.com/r/{subreddit}/top.json?sort=top&t=week&limit={limit}"
 default_subreddits = ["pics", "videos"]
 if "SUBREDDITS" in os.environ:
     default_subreddits = os.getenv("SUBREDDITS").split(",")
 POST_LIMIT = os.getenv("POST_LIMIT", 10)
+USER_AGENT = os.getenv("USER_AGENT")
+REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
+REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
 
 
 def lambda_handler(event, context):
     if not sender_mail or not recipients_mails:
         return format_response(400, "The sender or destination mail is not set.")
     mail_content = ""
+
+    logger.info("Trying to get a new access token.")
+    token_url = "https://www.reddit.com/api/v1/access_token"
+    payload = "grant_type=client_credentials"
+    headers = {
+        "User-Agent": USER_AGENT,
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": f"Basic {base64.b64encode(REDDIT_CLIENT_ID + ":" + REDDIT_CLIENT_SECRET)}"
+    }
+    response = requests.post(token_url, headers=headers, data=payload)
+    if response.status_code != 200:
+        return format_response(response.status_code, f"Failed to get access token ({response.status_code}) with error: {e}")
+    access_token = response.json()["access_token"]
+    logger.info(f"Successfully retrieved new access token!")
+
     for subreddit in default_subreddits:
         subreddit_url = reddit_api_url.format(subreddit=subreddit, limit=POST_LIMIT)
-        result = requests.get(subreddit_url, headers={"User-agent": "your bot 0.1"})
+        mail_content += f"/r/{subreddit}:<br>"
+        logger.info(f"Requesting url: {subreddit_url}")
+        result = requests.get(subreddit_url, headers={"Authorization": f"Bearer {access_token}", "User-agent": USER_AGENT})
         if result.status_code != 200:
             logger.warning(f"Subreddit {subreddit} sent a {result.status_code} HTTP code.")
+            mail_content += f"Could not get posts for subreddit /r/{subreddit} with HTTP code {result.status_code}.<br><br>"
+            logger.warning(result.text)
             continue
         posts_json = result.json()["data"]["children"]
         posts = [Post(post) for post in posts_json]
-        mail_content += f"/r/{subreddit}:<br>"
         for post in posts:
             mail_content += f'<a href="{post.url}">{post.title}</a><br>'
         mail_content += "<br>"
 
     current_week_number = datetime.date(datetime.today()).isocalendar()[1]
 
-    # Try to send the email.
+    # Try to send the email
     try:
-        # Provide the contents of the email.
+        # Provide the contents of the email
+        logger.info(f"Sending mail to {recipients_mails}")
         ses_client.send_email(
             Destination={
                 "ToAddresses": recipients_mails,
